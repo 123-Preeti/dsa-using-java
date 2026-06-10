71f335b0c92ac70bac356584ca81b9a350a817a2
Preeti Jha <jhapreeti872@gmail.com>
2026-06-10 19:54:34 +0530

    array

--- java-basic-impl/src/com/array/SearchingElement.java
+++ java-basic-impl/src/com/array/SearchingElement.java
@@ -1,9 +1,32 @@
 package com.array;
 
-import java.util.Scanner;
-
 public class SearchingElement {
-    public static void main(String args[]) {
-        int a[] = {12, 34, 45, 6, 13}
+
+    public static void main(String[] args) {
+
+        int a[] = {12, 34, 56, 7, 89, 4};
+
+        int i = searchIndex(a, 56);
+
+        if (i == -1) {
+            System.out.println("value dne");
+        } else {
+            System.out.println(i);
+        }
+    }
+
+    public static int searchIndex(int a[], int val) {
+
+        if (a.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == val) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
